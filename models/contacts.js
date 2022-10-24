15c6db4eb9deb599e6377d0d1e766e858f07f13d
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-10-24 22:41:34 +0300

    add delete, post and put functions

--- models/contacts.js
+++ models/contacts.js
@@ -15,17 +15,58 @@ const listContacts = async () => {
 const getContactById = async (contactId) => {
   try {
     const data = await fs.readFile(contactPath, "utf-8");
-    return JSON.parse(data).filter((item) => item.id === contactId);    
+    return JSON.parse(data).filter((item) => item.id === contactId);
   } catch (error) {
     console.log(error.message);
   }
 }
 
-const removeContact = async (contactId) => {}
+const removeContact = async (contactId) => {
+  try {
+    const data = await fs.readFile(contactPath, "utf-8");
+    const newList = await JSON.parse(data).filter((item) => item.id !== contactId);
+    fs.writeFile(contactPath, JSON.stringify(newList));
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-const addContact = async (body) => {}
+const addContact = async ({name, email, phone}) => {
+  try {
+    const data = await fs.readFile(contactPath, "utf-8");
+    const newContact = await {
+      id: Date.now().toString(),
+      name: name,
+      email: email,
+      phone: phone,
+    };
+    const newList = await [...JSON.parse(data), newContact];
+    fs.writeFile(contactPath, JSON.stringify(newList));
+    return newContact;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
 
-const updateContact = async (contactId, body) => {}
+const updateContact = async (contactId, body) => {
+  const { name, email, phone } = body;
+  const newList = [];
+  try {
+    const data = await fs.readFile(contactPath, "utf-8");
+    await JSON.parse(data).forEach((item) => {
+      if (item.id === contactId) {
+        item.name = name;
+        item.email = email;
+        item.phone = phone;
+      }
+      newList.push(item);
+    });
+    fs.writeFile(contactPath, JSON.stringify(newList));
+    return newList.filter((item) => item.id === contactId);
+  } catch (error) {
+    console.log(error.message);
+  }
+};
 
 module.exports = {
   listContacts,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const {listContacts, getContactById} = require("../../models/contacts.js");
+const {listContacts, getContactById, removeContact, addContact, updateContact} = require("../../models/contacts.js");
 
 
 const router = express.Router()
@@ -7,7 +7,7 @@ const router = express.Router()
 router.get('/', async (req, res, next) => {
   try {
     const response = await listContacts();
-    res.json({ response, status:'success' });
+    res.status(200).json({ response });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -16,22 +16,40 @@ router.get('/', async (req, res, next) => {
 router.get('/:contactId', async (req, res, next) => {
   try {
     const response = await getContactById(req.params.contactId);
-    res.json({ response, status: "success" });
+        if (!response[0]) {
+          return res.status(404).json({ message: "Not found!" });
+        }
+    res.status(200).json({ response });
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 })
 
 router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message post' })
+  try {
+    const response = await addContact(req.body);
+    res.status(201).json({ response });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message delete' })
+  try {
+    await removeContact(req.params.contactId);
+    res.status(200).json({ message: "contact deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message put' })
+  try {
+    const response = await updateContact(req.params.contactId, req.body);
+    res.status(200).json({ response });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 })
 
 module.exports = router
