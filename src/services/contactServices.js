5b4d1d2133b0757ff6965e8788e412807198df1f
Vitaliy Zhuravel <104020905+ikarkam76@users.noreply.github.com>
2022-11-09 21:49:04 +0200

    correct paginations limit

--- src/services/contactServices.js
+++ src/services/contactServices.js
@@ -2,7 +2,7 @@ const { Contact } = require("../models/contactModel");
 
 const getContacts = async (req, res) => {
   const { _id } = req.user;
-  const { page = 1, limit = 2 } = req.query;
+  const { page = 1, limit = 20 } = req.query;
   const skip = (page - 1) * limit;
   const data = await Contact.find({owner: _id}, '', {skip, limit}).populate('owner', 'email');
   return data;
